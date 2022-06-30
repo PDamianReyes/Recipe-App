6cdd994e5b44cd5042f5d87f6964948453b753b2
PEDRO_DAMIAN_REYES_CORTEZ <pedrodamianreyescortez@gmail.com>
2022-06-30 17:41:20 -0500

    Avance

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const mealsEl = document.getElementById("meals")
 const favoriteContainer = document.getElementById("fav-meals")
+const mealPopup = document.getElementById('meal-popup');
+const mealInfoEl = document.getElementById("meal-info");
+const popupCloseBtn = document.getElementById('close-popup');
 
 const searchTerm = document.getElementById ("fav-meals");
 const searchBtn = document.getElementById('search');
@@ -78,6 +81,10 @@ function addMeal(mealData, random = false) {
         fetchFavMeals();
     });
 
+    meal.addEventListener('click', () => {
+        showMealInfo(mealData);
+    })
+
     mealsEl.appendChild(meal);
 }
 
@@ -146,6 +153,37 @@ function addMealFav(mealData) {
     favoriteContainer.appendChild(favMeal);
 }
 
+function showMealInfo(mealData) {
+    //update the Meal Info
+    const mealEl = document.createElement('div');
+
+    mealEl.innerHTML = `
+        <h1>${mealData.strMeal}</h1>
+        <img
+            src="${mealData.strMealThumb}"
+            alt="${mealData.strMeal}"
+        />
+        <p>
+        ${mealData.strInstructions}
+        </p>
+        <h3>Ingredients:</h3>
+        <ul>
+            ${ingredients
+                .map(
+                    (ing) => `
+            <li>${ing}</li>
+            `
+                )
+                .join("")}
+        </ul>
+    `; 
+
+    mealInfoEl.appendChild(mealEl);
+
+    //Show the popup
+    mealPopup.classList.remove('hidden');
+}
+
 searchBtn.addEventListener("click", async () => {
     // clear container 
     mealsEl.innerHTML = '';
@@ -160,3 +198,7 @@ searchBtn.addEventListener("click", async () => {
     }
     
 });
+
+popupCloseBtn.addEventListener('click', () => {
+    mealPopup.classList.add('hidden');
+} )
